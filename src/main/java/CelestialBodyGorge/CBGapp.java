bf35d4471ea6f5cf941dcd99a79eba032da662f4
SaraLiWijnia <saraliwynia@gmail.com>
2019-04-01 13:13:36 +0200

    .
    .

--- src/main/java/CelestialBodyGorge/CBGapp.java
+++ src/main/java/CelestialBodyGorge/CBGapp.java
@@ -59,32 +59,34 @@ public class CBGapp extends GameEngine {
     }
     
     private void initializeTileMap () { 
-        Sprite grassSprite = new Sprite("src/main/java/CelestialBodyGorge/media/gras1.png");
+        Sprite grassSprite = new Sprite("src/main/java/CelestialBodyGorge/media/grond/gras1.png");
         TileType<BoardsTile> boardTileGrass = new TileType<>(BoardsTile.class, grassSprite);
         
-        Sprite aardeSprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
+        Sprite aardeSprite = new Sprite("src/main/java/CelestialBodyGorge/media/grond/aarde1.png");
         TileType<BoardsTile> boardTileAarde = new TileType<>(BoardsTile.class, aardeSprite);
         
-        Sprite geschoffeldSprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
+        Sprite geschoffeldSprite = new Sprite("src/main/java/CelestialBodyGorge/media/grond/geschoffeldeGrond.png");
         TileType<BoardsTile> boardTileGeschoffeld = new TileType<>(BoardsTile.class, geschoffeldSprite);
         
-        Sprite zaadjeSprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
-        TileType<BoardsTile> boardTileZaadje = new TileType<>(BoardsTile.class, zaadjeSprite);
-        
-        Sprite roos1Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
-        TileType<BoardsTile> boardTileRoos1 = new TileType<>(BoardsTile.class, roos1Sprite);
-        
-        Sprite roos2Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
-        TileType<BoardsTile> boardTileRoos2 = new TileType<>(BoardsTile.class, roos2Sprite);
-        
-        Sprite aardbei1Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
-        TileType<BoardsTile> boardTileAardbei1 = new TileType<>(BoardsTile.class, aardbei1Sprite);
-        
-        Sprite aardbei2Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
-        TileType<BoardsTile> boardTileAardbei2 = new TileType<>(BoardsTile.class, aardbei2Sprite);
-    	
-        TileType[] tileTypes = {boardTileGrass, boardTileAarde, boardTileGeschoffeld, boardTileZaadje,
-        		boardTileRoos1, boardTileRoos2, boardTileAardbei1, boardTileAardbei2};
+//        Sprite zaadjeSprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
+//        TileType<BoardsTile> boardTileZaadje = new TileType<>(BoardsTile.class, zaadjeSprite);
+//        
+//        Sprite roos1Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
+//        TileType<BoardsTile> boardTileRoos1 = new TileType<>(BoardsTile.class, roos1Sprite);
+//        
+//        Sprite roos2Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
+//        TileType<BoardsTile> boardTileRoos2 = new TileType<>(BoardsTile.class, roos2Sprite);
+//        
+//        Sprite aardbei1Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
+//        TileType<BoardsTile> boardTileAardbei1 = new TileType<>(BoardsTile.class, aardbei1Sprite);
+//        
+//        Sprite aardbei2Sprite = new Sprite("src/main/java/CelestialBodyGorge/media/aarde1.png");
+//        TileType<BoardsTile> boardTileAardbei2 = new TileType<>(BoardsTile.class, aardbei2Sprite);
+//    	
+      TileType[] tileTypes = {boardTileGrass, boardTileAarde, boardTileGeschoffeld
+    		  
+      };
+//        		boardTileZaadje, boardTileRoos1, boardTileRoos2, boardTileAardbei1, boardTileAardbei2};
         // 0 t/m 7
         int tileSize = TILESIZE;
         int tilesMap[][] = {
