b051a5cd406277219c821e49a77fcdd3ff8e8bf1
EStokhof <emma.11@live.nl>
2019-04-01 15:13:40 +0200

    rest

--- src/main/java/CelestialBodyGorge/Planten.java
+++ src/main/java/CelestialBodyGorge/Planten.java
@@ -1,18 +1,21 @@
 package CelestialBodyGorge;
 
 import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
 import nl.han.ica.oopg.tile.TileMap;
 
-public abstract class Planten {
+public abstract class Planten extends Tile {
 	protected int stadium;
 	protected int x;
 	protected int y;
-	protected TileMap tileMap;
+	protected Tile tegel;
 	
 	protected final int MAXSTADIUM = 3;
 	
-	public Planten(TileMap tileMap) {
-		this.tileMap = tileMap;
+	public Planten(Sprite sprite, int x, int y) {
+		super(sprite);
+		this.x = x;
+		this.y = y;
 	}
 	
 	public void setStadium() {
