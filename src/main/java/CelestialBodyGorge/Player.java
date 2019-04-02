82833b31369d5bea610e021d3aa6b15ae3fdf8b8
SaraLiWijnia <saraliwynia@gmail.com>
2019-04-02 09:54:57 +0200

    Player kan niet uit de map

--- src/main/java/CelestialBodyGorge/Player.java
+++ src/main/java/CelestialBodyGorge/Player.java
@@ -139,7 +139,7 @@ public class Player extends AnimatedSpriteObject {
 		float huidigePositie = this.getX();
 		float nieuwePositie = huidigePositie + stepSize;
 
-		if (nieuwePositie < world.getWORLDWIDTH() && nieuwePositie > 0) {
+		if (nieuwePositie <= world.getWORLDWIDTH() && nieuwePositie >= 0) {
 			// als de volgende stap in de wereld zit
 			if (getTileOnNextPlayerPosition(nieuwePositie, this.getY()).getLoopbaar() == true) {
 				// check of de volgende tile loopbaar is
@@ -154,7 +154,7 @@ public class Player extends AnimatedSpriteObject {
 		float huidigePositie = this.getY();
 		float nieuwePositie = huidigePositie + stepSize;
 
-		if (nieuwePositie < world.getWORLDHEIGHT() && nieuwePositie > 0) {
+		if (nieuwePositie <= world.getWORLDHEIGHT() && nieuwePositie >= 0) {
 			// als de volgende stap in de wereld zit
 			if (getTileOnNextPlayerPosition(this.getX(), nieuwePositie).getLoopbaar() == true) {
 				// check of de volgende tile loopbaar is
