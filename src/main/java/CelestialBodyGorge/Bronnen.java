0e30a9b6c7c01ab39b4ca21351c9f58e0b874c57
EStokhof <emma.11@live.nl>
2019-04-03 13:18:50 +0200

    delete bronnen

--- src/main/java/CelestialBodyGorge/Bronnen.java
+++
@@ -1,9 +0,0 @@
-package CelestialBodyGorge;
-
-public class Bronnen {
-	// https://www.glitchthegame.com/downloads/ staat de background music.
-	// Celianna's Parallax Tiles 
-	// Stardew Valley voor de planten 
-	// https://thestoryteller01.files.wordpress.com/2014/07/vx-plants-tileset1.png voor de tile van leaves
-	
-}
